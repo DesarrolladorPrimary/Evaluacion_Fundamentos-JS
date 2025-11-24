74399ca6777453a070df6f7ee8d2414e4e81fa0e
Keyner Arismendy <keinerf588@gmail.com.com>
2025-11-24 17:09:50 -0500

    correcion de errores en el ejercicio y mejoras

--- modules/modulos_ej1.js
+++ modules/modulos_ej1.js
@@ -1,29 +1,18 @@
-
-let promedio = 0;
-let categoria = "";
-
-export const calcularPromedio = (notas) => {
+export const calcularPromedio = function(notas) {
   let suma = 0;
   for (let i = 0; i < notas.length; i++) {
-    suma += notas[i];
+    suma += parseFloat(notas[i]);
   }
-  return promedio = suma / notas.length;
+  return suma / notas.length;
 };
 
 
-export const nivelRendimiento = () => {
- 
+export const nivelRendimiento = function(promedio) {
   if (promedio >= 4.5) {
-
-    return categoria = "Alto";
-
+    return "Alto";
   } else if (promedio < 4.5 && promedio >= 3.0) {
-
-    return  categoria = "Medio";
-
-  } else if (promedio < 3.0) {
-
-    return categoria = "Bajo";
-
+    return "Medio";
+  } else {
+    return "Bajo";
   }
 };
--- scripts/ej1.js
+++ scripts/ej1.js
@@ -2,16 +2,16 @@ import { calcularPromedio, nivelRendimiento } from "../modules/modulos_ej1.js";
 
 
 alert("Sistema de notas")
-let numNotas = prompt("Ingrese el numero de notas: ")
+let numNotas = parseInt(prompt("Ingrese el numero de notas: "));
 
 let notas = [];
 
 for (let i = 0; i < numNotas; i++) {
-    notas[i] = prompt("Ingrese la nota " + (i + 1) + ": ");
+    notas[i] = parseFloat(prompt("Ingrese la nota " + (i + 1) + ": "));
 }
 
 let promedio = calcularPromedio(notas);
-let categoria = nivelRendimiento();
+let categoria = nivelRendimiento(promedio);
 
 alert("El promedio final es: " + promedio.toFixed(2));
 alert("Categoria de rendimiento: " + categoria);
